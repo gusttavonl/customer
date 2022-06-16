bbfe8d0db28f5d0332d754341d389f9c1c5df0da
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:00:00 -0300

    feat: add http helper

---
+++ src/common/helpers/http.ts
@@ -0,0 +1,34 @@
+export type HttpResponse = {
+  statusCode: number;
+  body: any;
+};
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+});
+
+export const created = (data: any): HttpResponse => ({
+  statusCode: 201,
+  body: data
+});
+
+export const badRequest = (errorMessage: string): HttpResponse => ({
+  statusCode: 400,
+  body: errorMessage
+});
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: "Unauthorized"
+});
+
+export const notFound = (errorMessage: string): HttpResponse => ({
+  statusCode: 404,
+  body: errorMessage
+});
+
+export const notAvailable = (errorMessage?: string): HttpResponse => ({
+  statusCode: 502,
+  body: errorMessage
+});
