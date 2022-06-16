4d3a8e43521f685c83b5e6dab7b0047c6d8ebaab
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:10:17 -0300

    test: add get customer service test

---
+++ test/services/customer/get-customer.service.spec.ts
@@ -0,0 +1,47 @@
+import { Redis } from "ioredis";
+import { notFound } from "../../../src/common/helpers/http";
+import { RedisHelper } from "../../../src/common/helpers/redis";
+import { CreateCustomerService } from "../../../src/modules/customers/services/create-customer.service";
+import { GetCustomerService } from "../../../src/modules/customers/services/get-customer.service";
+
+const makeCustomerDomain = () => ({
+  name: "any_name",
+  document: 12345678910
+});
+
+describe("GetCustomerService", () => {
+  let redisClient: Redis;
+
+  beforeAll(async () => {
+    redisClient = await RedisHelper.getClient(false);
+  });
+
+  afterAll(async () => {
+    redisClient && (await redisClient.quit());
+  });
+
+  describe("get", () => {
+    it("should get a created customer", async () => {
+      const sutCreateCustomer = new CreateCustomerService();
+      const sutGetCustomer = new GetCustomerService();
+
+      const customerDataToCreate = makeCustomerDomain();
+      const customerCreatedHttpResponse = await sutCreateCustomer.create(
+        customerDataToCreate
+      );
+      const customerCreatedId = customerCreatedHttpResponse.body.id
+      const customerFindedHttpResponse = await sutGetCustomer.getById(customerCreatedId);
+      const customerFindedId = customerFindedHttpResponse.body.id
+      expect(customerCreatedId).toEqual(customerFindedId);
+    });
+
+    it("should return not found error", async () => {
+      const sutGetCustomer = new GetCustomerService();
+      const customerNotFoundHttpResponse = await sutGetCustomer.getById('any_id');
+      const NotFoundWithErrorMessage = notFound("customer not found")
+      expect(customerNotFoundHttpResponse.statusCode).toEqual(NotFoundWithErrorMessage.statusCode);
+      expect(customerNotFoundHttpResponse.body).toEqual(NotFoundWithErrorMessage.body);
+    });
+
+  });
+});
