0cddc0ca7dc0b48f2f4804695480368d0a7a5f86
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:00:53 -0300

    feat: add create customer service

---
+++ src/modules/customers/services/create-customer.service.ts
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from "uuid";
+import { Injectable } from "@nestjs/common";
+import { CustomerDomain } from "../domain/customer.domain";
+import { CreateCustomerServiceInterface } from "../interfaces/services/create-customer-service.interface";
+import { RedisHelper } from "../../../common/helpers/redis";
+import { Customer } from "../domain/customer.entity";
+import { created, HttpResponse } from "../../../common/helpers/http";
+
+@Injectable()
+export class CreateCustomerService implements CreateCustomerServiceInterface {
+  constructor() {}
+
+  async create(customerDomain: CustomerDomain): Promise<HttpResponse> {
+    const redisClient = await RedisHelper.getClient();
+
+    const uuidCustomer = uuidv4();
+    const newKeyCustomerWithUuid = `customers:${uuidCustomer}`;
+
+    const customerToCreate = {
+      id: uuidCustomer,
+      ...customerDomain
+    };
+
+    await redisClient.set(newKeyCustomerWithUuid, JSON.stringify(customerToCreate));
+    
+    const createdCustomer = await redisClient.get(newKeyCustomerWithUuid);
+
+    return created(JSON.parse(createdCustomer) as Customer)
+  }
+}
