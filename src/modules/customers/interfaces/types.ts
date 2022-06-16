a3fd79f1fcf37364703381facb481246bd8fd80d
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:03:08 -0300

    feat: types interface

---
+++ src/modules/customers/interfaces/types.ts
@@ -0,0 +1,12 @@
+export const TYPES = {
+  services: {
+    CreateCustomerServiceInterface: "CreateCustomerServiceInterface",
+    GetCustomerServiceInteface: "GetCustomerServiceInteface",
+    UpdateCustomerServiceInterface: "UpdateCustomerServiceInterface"
+  },
+  applications: {
+    CreateCustomerApplicationInterface: "CreateCustomerApplicationInterface",
+    GetCustomerApplicationInterface: "GetCustomerApplicationInterface",
+    UpdateCustomerApplicationInterface: "UpdateCustomerApplicationInterface"
+  }
+};
