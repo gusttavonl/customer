ddcd5e7a8e203c632475499aad215cf9556203b3
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:07:33 -0300

    test: add validation helper test

---
+++ test/helpers/validation.spec.ts
@@ -0,0 +1,78 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsNumber,
+} from 'class-validator';
+import { ValidationPipe } from '../../src/common/helpers/validation';
+import { ApiProperty } from '@nestjs/swagger';
+import { badRequest } from '../../src/common/helpers/http';
+
+class TestDomain {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  document: number;
+
+}
+
+describe('ValidationPipe', () => {
+  let validationPipe: ValidationPipe;
+  const customer: TestDomain = {
+    name: 'any_name',
+    document: 12345678910,
+  };
+
+  describe('when data is not valid', () => {
+    it('should throw error', done => {
+      validationPipe = new ValidationPipe(TestDomain);
+      const newCustomer = {
+        name: 'any_name',
+        document: 'document_string',
+      };
+
+      validationPipe
+        .transform(newCustomer, {
+          data: '',
+          type: 'body',
+          metatype: TestDomain,
+        })
+        .then(() => done('Error, Should not get here'))
+        .catch(error => {
+          expect(error.response.message).toEqual('Validation Failed');
+          expect(error.status).toBe(badRequest(error).statusCode);
+          done();
+        });
+    });
+     
+  });
+
+  describe('when no transform needed', () => {
+    it('should get the data', async () => {
+      validationPipe = new ValidationPipe(TestDomain);
+      const obj = { test: 'any' };
+      const value = await validationPipe.transform(obj, {
+        data: '',
+        type: 'body',
+        metatype: String,
+      });
+      expect(value).toEqual(obj);
+    });
+  });
+
+  describe('when send domain object', () => {
+    it('needs to validate', async () => {
+      validationPipe = new ValidationPipe(TestDomain);
+      const value = await validationPipe.transform(customer, {
+        data: '',
+        type: 'body',
+        metatype: TestDomain,
+      });
+      expect(value).toEqual(customer);
+    });
+  });
+});
