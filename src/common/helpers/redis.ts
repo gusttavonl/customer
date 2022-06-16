e219a00db5f46148e98e303c35c5dd3718daa1b8
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:00:10 -0300

    feat: add redis helper

---
+++ src/common/helpers/redis.ts
@@ -0,0 +1,22 @@
+import Redis, { Redis as RedisClient } from "ioredis";
+import { notAvailable } from "./http";
+
+export class RedisHelper {
+  static redisClient: RedisClient;
+
+  static async getClient(redisTestToFailed?: boolean): Promise<any> {
+    const noHost = 'no_host'
+    const redisConnection = new Redis({
+      host: redisTestToFailed ? noHost : process.env.REDIS_HOST ,
+      port: 6379,
+      db: 0
+    });
+
+    if(redisConnection.options.host === noHost || !redisConnection.options.host){
+      return notAvailable("redis not available");
+    }
+
+    this.redisClient = redisConnection
+    return this.redisClient;
+  }
+}
