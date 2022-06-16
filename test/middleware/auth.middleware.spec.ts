9149e2571d31ec02eb79d6ad3252f2e340026229
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:07:19 -0300

    test: add auth middleware test

---
+++ test/middleware/auth.middleware.spec.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../../src/app.module';
+import { INestApplication } from '@nestjs/common';
+
+describe('AppController', () => {
+  let app: INestApplication;
+  let mockAxios: MockAdapter;
+
+  beforeAll(async () => {
+    mockAxios = new MockAdapter(axios);
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it(`Should return 200 if SSO is available`, async () => {
+    mockAxios.onPost(process.env.KEYGLOACK_INSTROSPECT_URL).reply(200, { active: true });
+
+    return await request(app.getHttpServer())
+      .post('/customer')
+      .send({
+        name: 'any_name',
+        document: 12345678910,
+      })
+      .set('Authorization', `Bearer any_token`)
+      .expect(201);
+  });
+
+  it(`Should return 401 if token is not active`, async () => {
+    mockAxios.onPost(process.env.KEYGLOACK_INSTROSPECT_URL).reply(200, { active: false });
+
+    return await request(app.getHttpServer())
+      .post('/customer')
+      .send({
+        name: 'any_name',
+        document: 12345678910,
+      })
+      .set('Authorization', 'Bearer any_token')
+      .expect(401);
+  });
+
+  it(`Should return 502 if SSO is not available`, async () => {
+    mockAxios.onPost(process.env.KEYGLOACK_INSTROSPECT_URL).reply(500);
+
+    return await request(app.getHttpServer())
+      .post('/customer')
+      .send({
+        name: 'any_name',
+        document: 12345678910,
+      })
+      .set('Authorization', 'Bearer any_token')
+      .expect(502);
+  });
+});
