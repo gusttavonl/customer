c7deea0607fabaecf6e1acd89d75c32e08b8018e
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 11:05:28 -0300

    feat: add create customer controller

---
+++ src/modules/customers/controller/create-customer.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Inject, Post, Body, UsePipes, Res } from "@nestjs/common";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
+import { CustomerDomain } from "../domain/customer.domain";
+import { TYPES } from "../interfaces/types";
+import { CreateCustomerApplicationInterface } from "../interfaces/applications/create-customer.application.interface";
+import { Customer } from "../domain/customer.entity";
+import { Response } from "express";
+import { ValidationPipe } from "../../../common/helpers/validation";
+
+@Controller("customer")
+@ApiTags("Customer")
+export class CreateCustomerController {
+  constructor(
+    @Inject(TYPES.applications.CreateCustomerApplicationInterface)
+    private createCustomerApplication: CreateCustomerApplicationInterface
+    ) {}
+
+  @ApiCreatedResponse({
+    description: "It creates a new customer",
+    type: Customer
+  })    
+  @UsePipes(new ValidationPipe(CustomerDomain))
+  @Post()
+  async create(@Body() customerDomain: CustomerDomain, @Res() res: Response) {
+    const response = await this.createCustomerApplication.create(customerDomain);
+    return res.status(response.statusCode).json(response.body);
+  }
+}
