9c414d1905cc0b98acd083df7ec5bb1676089c41
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-16 10:59:03 -0300

    chore: add initial config files to project

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { CustomersModule } from "./modules/customers/customers.module";
+import { AuthMiddleware } from "./modules/middleware/auth.middleware";
+
+@Module({
+  imports: [ConfigModule.forRoot(), CustomersModule]
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes("*");
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder } from "@nestjs/swagger/dist/document-builder";
+import { SwaggerModule } from "@nestjs/swagger/dist/swagger-module";
+import { AppModule } from "./app.module";
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle("Customer API")
+    .setDescription("Customer API to managaner customers")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("docs", app, document);
+
+  const port = 3000
+  await app.listen(port);
+  Logger.log(`Appplication started on port: ${port}`);
+}
+bootstrap();
