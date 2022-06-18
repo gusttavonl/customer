8bf88792696c1f7aa1f909159ebce824b131717c
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-06-18 17:15:58 -0300

    feat: add customer domain with id

--- src/modules/customers/domain/customer.domain.ts
+++ src/modules/customers/domain/customer.domain.ts
@@ -1,7 +1,25 @@
-import { IsString, IsNumber, IsNotEmpty } from "class-validator";
+import { IsString, IsNumber, IsNotEmpty, IsUUID } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CustomerDomain {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  document: number;
+}
+
+export class CustomerDomainWithId {
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID(4, { each:true })
+  @ApiProperty()
+  id: string;
+
   @IsNotEmpty()
   @IsString()
   @ApiProperty()
